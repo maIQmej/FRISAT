0feb006c4a9cb0e748bc39f8eb2806ba789accb2
Miguel aguirre <aguirremiguelmejia@gmail.com>
2025-07-04 00:47:55 +0000

    usa "back2.mp4"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function WelcomePage() {
         playsInline
         className="absolute inset-0 h-full w-full object-cover"
       >
-        <source src="/back.mp4" type="video/mp4" />
+        <source src="/back2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute inset-0 bg-blue-700 opacity-60 mix-blend-screen" />
